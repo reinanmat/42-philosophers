ec1311bb79ef85507d13aea937e36ea91dd9951c
reinanmat <92590079+reinanmat@users.noreply.github.com>
2023-04-30 13:25:21 -0300

    feat: added basic project structure

---
+++ philo/includes/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/30 13:30:00 by revieira          #+#    #+#             */
+/*   Updated: 2023/04/30 13:31:53 by revieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "pthread.h"
+#include "sys/time.h"
+
+#endif
