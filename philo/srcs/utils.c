a75ec40652acc4a7a62e24fabc449602323676f0
reinanmat <reinan266@gmail.com>
2023-05-04 17:41:03 -0300

    feat: added utils functions

---
+++ philo/srcs/utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 16:49:51 by revieira          #+#    #+#             */
+/*   Updated: 2023/05/04 17:25:38 by revieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	ft_isspace(char c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_isdigit(char c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
+size_t	ft_amount_of_digits(char *str)
+{
+	int	i;
+	int	amount;
+
+	i = 0;
+	amount = 0;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]))
+			amount++;
+		i++;
+	}
+	return (amount);
+}
+
+long long	ft_atoill(char *str)
+{
+	int			i;
+	int			sign;
+	long long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			sign = -1;
+	if (!ft_isdigit(str[i]))
+		return (0);
+	while(ft_isdigit(str[i]))
+	{
+		result = 10 * result + str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
