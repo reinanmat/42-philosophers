95988774c1394b44affb235bc2e5465af47290e4
reinanmat <reinan266@gmail.com>
2023-05-04 17:39:05 -0300

    feat: added helper functions for checking arguments

--- philo/srcs/check_args.c
+++ philo/srcs/check_args.c
@@ -12,6 +12,61 @@
 
 #include "../includes/philo.h"
 
+static int check_pos_numbers(char *number)
+{
+	int	i;
+
+	i = 0;
+	while (ft_isspace(number[i]))
+		i++;
+	if (number[i] == '-')
+	{
+		printf("Error: negative number\n");
+		return (0);
+	}
+	if (number[i] == '+')
+		i++;
+	while (ft_isdigit(number[i]))
+		i++;
+	while (ft_isspace(number[i]))
+		i++;
+	if (number[i] != '\0')
+	{
+		printf("Error: invalid number\n");
+		return (0);
+	}
+	return (1);
+}
+
+static int	check_int_max(char *str)
+{
+	long long	number;
+	size_t		digits;
+
+	digits = ft_amount_of_digits(str);
+	number = ft_atoill(str);
+	if (digits > 10 || number > 2147483647)
+	{
+		printf("Error: invalid number bigger then INT_MAX\n");
+		return (0);
+	}
+	return (1);
+}
+
+static int	check_numbers(char **argv, int (*funct)(char *))
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (!funct(argv[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	check_args(int argc, char **argv)
 {
 	if (argc != 5 && argc != 6)
