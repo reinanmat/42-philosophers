0add74b5e43e9900a7bfecdeab91d1a5469596c3
reinanmat <reinan266@gmail.com>
2023-05-04 17:33:39 -0300

    feat: added struct data

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 13:30:00 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/30 13:31:53 by revieira         ###   ########.fr       */
+/*   Updated: 2023/05/04 17:23:35 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,12 @@
 #include "pthread.h"
 #include "sys/time.h"
 
+typedef struct s_data
+{
+	int	numbers_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_time_each_philosopher_must_eat;
+}	t_data;
 #endif
