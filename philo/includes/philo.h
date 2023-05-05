e9d550cddea7077a062b99703f1e3f9d147b9036
reinanmat <reinan266@gmail.com>
2023-05-05 17:46:12 -0300

    refactor: new struct for philosophers

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 13:30:00 by revieira          #+#    #+#             */
-/*   Updated: 2023/05/04 17:45:00 by revieira         ###   ########.fr       */
+/*   Updated: 2023/05/05 16:47:19 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,23 @@
 
 typedef struct s_data
 {
-	int		numbers_of_philosophers;
+	int		nbr_of_philosophers;
 	int		time_to_die;
 	int		time_to_eat;
 	int		time_to_sleep;
-	int		number_of_time_each_philosopher_must_eat;
+	int		meal_numbers;
 }	t_data;
+
+typedef struct s_philo
+{
+	int				id;
+	int				last_meal;
+	t_data			*data;
+	pthread_t		th;
+	pthread_mutex_t	fork;
+	pthread_mutex_t *fork_left;
+	pthread_mutex_t	*on_print;
+}	t_philo;
 
 int			check_args(int argc, char **argv);
 
