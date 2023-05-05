44c25ce8f70b2d02c4594f7eee1db70b74187c66
reinanmat <reinan266@gmail.com>
2023-05-05 17:47:55 -0300

    feat: init_data

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,28 +6,31 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 15:15:02 by revieira          #+#    #+#             */
-/*   Updated: 2023/05/04 17:15:59 by revieira         ###   ########.fr       */
+/*   Updated: 2023/05/05 16:51:18 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	init_data(char **argv, t_data *data)
+void	init_data(int argc, char **argv, t_data *data)
 {
-	data = malloc(sizeof(t_data));
-	data->numbers_of_philosophers = (int)ft_atoill(argv[0]);
-	data->time_to_die = (int)ft_atoill(argv[1]);
-	data->time_to_eat = (int)ft_atoill(argv[2]);
-	data->time_to_sleep = (int)ft_atoill(argv[3]);
-	data->number_of_time_each_philosopher_must_eat = (int)ft_atoill(argv[4]);
+	data->nbr_of_philosophers = (int)ft_atoill(argv[1]);
+	data->time_to_die = (int)ft_atoill(argv[2]);
+	data->time_to_eat = (int)ft_atoill(argv[3]);
+	data->time_to_sleep = (int)ft_atoill(argv[4]);
+	if (argc == 6)
+		data->meal_numbers = (int)ft_atoill(argv[5]);
+	else
+		data->meal_numbers = -1;
+}
+
 }
 
 int	main(int argc, char **argv)
 {
-	t_data	*data;
+	t_data	data;
 
-	data = NULL;
 	if (!check_args(argc, argv))
 		return (1);
-	init_data(argv, data);
+	init_data(argc, argv, &data);
 }
