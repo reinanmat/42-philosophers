f0ab241fdd915010348b2ff9df4a83c2f8a6af20
reinanmat <reinan266@gmail.com>
2023-05-08 19:04:14 -0300

    fix: initialize last_meal

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -94,6 +94,7 @@ t_philo	*init_philosophers(t_data *data)
 		philo[i].id = i + 1;
 		philo[i].data = data;
 		philo[i].on_print = on_print;
+		philo[i].last_meal = 0;
 		pthread_mutex_init(&philo[i].fork, NULL);
 		i++;
 	}
