ba6a2088c8ea651ac15cf2c0ac2b492282a0a00a
reinanmat <reinan266@gmail.com>
2023-05-09 18:24:08 -0300

    fix: error compilation

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 15:15:02 by revieira          #+#    #+#             */
-/*   Updated: 2023/05/09 17:52:02 by revieira         ###   ########.fr       */
+/*   Updated: 2023/05/09 18:23:34 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,9 +95,7 @@ void	free_struct(t_philo *philo)
 		pthread_mutex_destroy(&philo[i].fork);
 		i++;
 	}
-	if (philo)
-		free(philo);
-}
+	free(philo);
 }
 
 int	main(int argc, char **argv)
