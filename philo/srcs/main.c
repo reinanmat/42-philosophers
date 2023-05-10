2cf7450afea521fc347fe1e132272fc7edef0374
reinanmat <reinan266@gmail.com>
2023-05-10 12:21:26 -0300

    feat: added only_one_philo

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 15:15:02 by revieira          #+#    #+#             */
-/*   Updated: 2023/05/09 18:23:34 by revieira         ###   ########.fr       */
+/*   Updated: 2023/05/10 12:20:49 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,6 +96,17 @@ void	free_struct(t_philo *philo)
 		i++;
 	}
 	free(philo);
+}
+
+int	only_one_philo(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->fork);
+	print_actions(philo, TAKEN_FORK);
+	usleep(philo->data->time_to_die * 1000);
+	print_actions(philo, DIED);
+	pthread_mutex_unlock(&philo->fork);
+	free_struct(philo);
+	return (0);
 }
 
 int	main(int argc, char **argv)
