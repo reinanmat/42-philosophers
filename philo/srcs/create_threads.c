f869f3a734bc91bc4e99fa82b1c5dbc0fa9456f9
reinanmat <reinan266@gmail.com>
2023-05-10 17:51:29 -0300

    feat: added monitoring function

--- philo/srcs/create_threads.c
+++ philo/srcs/create_threads.c
@@ -6,16 +6,39 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:45:02 by revieira          #+#    #+#             */
-/*   Updated: 2023/05/10 12:17:39 by revieira         ###   ########.fr       */
+/*   Updated: 2023/05/10 14:57:06 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+static int	monitoring(t_philo *philo, t_data *data)
+{
+	int		i;
+	time_t	current_time;
+
+	while (1)
+	{
+		i = 0;
+		while (i < data->nbr_of_philosophers)
+		{
+			current_time = get_time();	
+			if (current_time - philo[i].last_meal >= data->time_to_die)
+			{
+				pthread_mutex_lock(philo[i].mstop);
+				philo[i].stop[0] = 0;
+				pthread_mutex_unlock(philo[i].mstop);
+				print_action(&philo[i], DIED);
+				return (1);
+			}
+			i++;
+		}
+	}
+}
+
 void	create_threads(t_philo *philo, t_data *data)
 {
-	int			i;
-	pthread_t	th_monitoring;
+	int	i;
 
 	i = 0;
 	while (i < data->nbr_of_philosophers)
@@ -24,12 +47,5 @@ void	create_threads(t_philo *philo, t_data *data)
 		pthread_create(&philo[i].th, NULL, &routine, (void *)&philo[i]);
 		i++;
 	}
-	pthread_create(&th_monitoring, NULL, &monitoring, (void *)philo);
-	i = 0;
-	while (i < data->nbr_of_philosophers)
-	{
-		pthread_join(philo[i].th, NULL);
-		i++;
-	}
-	pthread_join(th_monitoring, NULL);
+	monitoring(philo, data);
 }
