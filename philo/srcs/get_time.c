9317a106e498e276bb20e554045874e85dacad99
reinanmat <reinan266@gmail.com>
2023-05-12 17:54:15 -0300

    fix: removed functions that convert time to ms

--- philo/srcs/get_time.c
+++ philo/srcs/get_time.c
@@ -6,28 +6,18 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 12:43:58 by revieira          #+#    #+#             */
-/*   Updated: 2023/05/12 14:32:10 by revieira         ###   ########.fr       */
+/*   Updated: 2023/05/12 17:29:25 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-static time_t	seconds_to_ms(time_t seconds)
-{
-	return (seconds * 1000);
-}
-
-static time_t	microseconds_to_ms(time_t miliseconds)
-{
-	return (miliseconds / 1000);
-}
-
 time_t	get_time(void)
 {
-	struct timeval tv;
+	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return (seconds_to_ms(tv.tv_sec) + microseconds_to_ms(tv.tv_usec));
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
 time_t	get_timestamp(t_philo *philo)
