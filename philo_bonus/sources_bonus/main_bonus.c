aada8e72bc131924b08b7140ae3d71670063c1cc
reinanmat <reinan266@gmail.com>
2023-05-13 18:27:52 -0300

    feat: added free_struct

--- philo_bonus/sources_bonus/main_bonus.c
+++ philo_bonus/sources_bonus/main_bonus.c
@@ -6,11 +6,20 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 11:41:16 by revieira          #+#    #+#             */
-/*   Updated: 2023/05/13 12:57:08 by revieira         ###   ########.fr       */
+/*   Updated: 2023/05/13 18:23:57 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes_bonus/philo_bonus.h"
+
+void	free_struct(t_philo *philo)
+{
+	sem_unlink(FORKS);
+	sem_unlink(PRINT);
+	sem_close(philo->table->forks_in_table);
+	free(philo->table);
+	free(philo);
+}
 
 int	main(int argc, char **argv)
 {
